f06fde688062120add2c538fbaed734f981e989e
Liliana Ramos <a01644969@tec.mx>
2025-09-18 20:29:23 -0600

    Diseño de Forgot Password

--- frontend/src/navigation/AuthNavigator.tsx
+++ frontend/src/navigation/AuthNavigator.tsx
@@ -5,11 +5,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "../screens/auth/LoginScreen";
 import RegisterScreen from "../screens/auth/RegisterScreen";
 import AvisoPrivacidadScreen from "../screens/auth/AvisoPrivacidadScreen";
+import ForgotPasswordScreen from "../screens/auth/ForgotPasswordScreen";
 
 export type RootStackParamList = {
   Login: undefined;
   Register: undefined;
   AvisoPrivacidad: undefined;
+  ForgotPassword: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -20,6 +22,7 @@ const AuthNavigator = () => {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="AvisoPrivacidad" component={AvisoPrivacidadScreen} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
     </Stack.Navigator>
   );
 };
