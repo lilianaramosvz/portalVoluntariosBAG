b57aaac9fa76d34dba315fc30a4e83fab1bb2bdb
Liliana Ramos <a01644969@tec.mx>
2025-09-25 19:01:15 -0600

    Flujo en dashboard admin

--- frontend/src/screens/admin/ListaVoluntariosScreen.tsx
+++ frontend/src/screens/admin/ListaVoluntariosScreen.tsx
@@ -1,13 +1,22 @@
 //frontend\src\screens\admin\ListaVoluntariosScreen.tsx
 
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "../../styles/screens/admin/ListaVoluntariosStyles";
+import { HeaderBack } from "../../components/headerTitle";
+import { useNavigation } from "@react-navigation/native";
 
 const ListaVoluntariosScreen: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Lista de Voluntarios</Text>
+      <HeaderBack 
+        title="Voluntarios registrados" 
+        onBack={() => navigation.goBack()}
+      />
+      <View style={styles.divisorline} />
+      
     </View>
   );
 };
--- frontend/src/screens/admin/SolicitudesScreen.tsx
+++ frontend/src/screens/admin/SolicitudesScreen.tsx
@@ -1,14 +1,24 @@
 //frontend\src\screens\admin\SolicitudesScreen.tsx
 
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from "../../styles/screens/admin/SolicitudesStyles";
+import { HeaderBack } from "../../components/headerTitle";
+import { useNavigation } from "@react-navigation/native";
 
 const SolicitudesScreen: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Solicitudes Screen</Text>
-    </View>
+      <HeaderBack 
+        title="Solicitudes de voluntarios" 
+        onBack={() => navigation.goBack()}
+      />
+      <View style={styles.divisorline} />
+      
+        {/* Aquí irá tu contenido de solicitudes */}
+      </View>
   );
 };
 
