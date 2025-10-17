896a2fc8ef6be3e178487be7a79cb60fd264b1d5
goliat2020 <alejandroguza44@hotmail.com>
2025-10-16 21:14:53 -0600

    Archivos innecesarios eliminados

--- start-emulators.ps1
+++
@@ -1,7 +0,0 @@
-# start-emulators.ps1
-# PowerShell helper to start Firebase emulators for this repo
-# Usage: Open PowerShell, run: .\start-emulators.ps1
-
-Write-Host "Starting Firebase emulators (functions, firestore, auth)..."
-cd $PSScriptRoot
-firebase emulators:start --only functions,firestore,auth
