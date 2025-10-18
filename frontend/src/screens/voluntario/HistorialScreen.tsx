feeecb82e966b373355ea977a37641d44d8c59f6
ivanabanderas05 <banderasivana@gmail.com>
2025-10-17 19:45:29 -0600

    Final Diseño Historial Screen

--- frontend/src/screens/voluntario/HistorialScreen.tsx
+++ frontend/src/screens/voluntario/HistorialScreen.tsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { styles } from "../../styles/screens/voluntario/HistorialStyles";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { HeaderBack } from "../../components/headerTitle";
 
 export default function HistorialScreen() {
   const navigation = useNavigation();
+
+
+  //Aqui tendria que ir la logica de como tendra los datos
+  const historial = [
+    {
+        fecha: "Jueves, 4 de septiembre de 2025",
+        entradas :[{ hora: "12:30", tipo: "Entrada" }, { hora: "14:00", tipo: "Entrada" }],
+    },
+    {
+        fecha: "Viernes, 5 de septiembre de 2025", 
+        entradas: [{hora: "11:15" ,tipo: "Entrada"} , { hora: "14:00", tipo: "Entrada" }],
+    },
+
+        {
+        fecha: "Miercoles, 3 de septiembre de 2025", 
+        entradas: [{hora: "09:24" ,tipo: "Entrada"} , { hora: "06:23", tipo: "Entrada" }],
+    },
+]
 
   return (
     <SafeAreaView style={styles.container}>
@@ -22,8 +41,30 @@ export default function HistorialScreen() {
           <Text style={styles.description}>Días de voluntariado registrados</Text>
         </View>
 
-        {/* Espaciador para las cajas del medio */}
-        <View style={{ flex: 1 }} />
+        <ScrollView style={styles.scrollContainer}>
+            {historial.map((dia,index) => (
+                <View key={index} style={styles.formDia}>
+                    <View style={styles.row}>
+                        <Ionicons name="calendar-clear-outline" size={18} style={styles.icon}/>
+                        <Text style={[styles.description, { marginLeft: 5 }]}>{dia.fecha}</Text>
+                    </View>
+                    {dia.entradas.map((entrada,idx) => (
+                        <View key={idx} style={styles.formGray}>
+                            <View style={styles.row}>
+                                <View style={styles.rowLeft}>
+                                    <Ionicons name="time-outline" size={18} style={styles.icon}/>
+                                    <Text style={[styles.hora, { marginLeft: 5 }]}>{entrada.hora}</Text>
+                                </View>
+                                <View style={styles.formEntrada}>
+                                    <Text style={styles.entrada}>{entrada.tipo}</Text>
+                                 </View>
+
+                            </View>
+                        </View>
+                    ))}
+                </View>
+            ))}
+        </ScrollView>
 
         {/* Última caja */}
         <View style={styles.formGreen}>
