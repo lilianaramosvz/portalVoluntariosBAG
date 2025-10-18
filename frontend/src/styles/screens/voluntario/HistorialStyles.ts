b2103b2bdb0eedc2baee3640a404c5628c5cd836
ivanabanderas05 <banderasivana@gmail.com>
2025-10-17 19:46:07 -0600

    estilos finales Historial Screen

--- frontend/src/styles/screens/voluntario/HistorialStyles.ts
+++ frontend/src/styles/screens/voluntario/HistorialStyles.ts
@@ -8,6 +8,7 @@ export const styles = StyleSheet.create({
   scrollContainer: {
     flex: 1,
     backgroundColor: Colors.background,
+    marginVertical: 30
   },
   container: {
     flex: 1,
@@ -23,6 +24,8 @@ export const styles = StyleSheet.create({
     width: "100%",
     marginBottom: 30,
   },
+
+  //forms, las cajas
     form: {
     backgroundColor: Colors.white,
     borderRadius: 16,
@@ -34,9 +37,10 @@ export const styles = StyleSheet.create({
     padding: 30,
     alignItems: "center",
     justifyContent: "center",
+    marginHorizontal: 5,
   },
-  formGreen: {
-    backgroundColor: "rgba(235, 255, 238, 0.8)",
+  formDia:{
+    backgroundColor: Colors.white,
     borderRadius: 16,
     elevation: 3,
     shadowColor: Colors.text,
@@ -46,7 +50,42 @@ export const styles = StyleSheet.create({
     padding: 30,
     alignItems: "center",
     justifyContent: "center",
+    marginBottom: 10,
+    marginHorizontal: 5,
+    marginTop: 4
   },
+  formGreen: {
+    backgroundColor: "rgba(235, 255, 238, 0.8)",
+    borderRadius: 16,
+    elevation: 3,
+    shadowColor: Colors.text,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    padding: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formGray: {
+    backgroundColor: Colors.lightGray,
+    borderRadius: 8,
+    padding: 15,
+    justifyContent: "center",
+    marginHorizontal: 30,
+    marginVertical: 5,
+    minWidth: 300
+  },
+  formEntrada: {
+    backgroundColor: Colors.primary,
+    borderRadius: 12,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    minWidth: 70
+  },
+
+  //textos
   titleGreen: {
     fontFamily: "Inter_600SemiBold",
     fontSize: 18,
@@ -63,6 +102,17 @@ export const styles = StyleSheet.create({
     lineHeight: 22,
     color: Colors.text,
   },
+  entrada: {
+    fontFamily: "Inter_600SemiBold", 
+    fontSize: 14,
+    color: Colors.white,
+  },
+  hora: {
+    fontFamily: "Inter_600SemiBold", 
+    fontSize: 18, 
+    fontWeight: "bold",
+    color: Colors.text
+  },
   number: {
     color: Colors.primary,
     fontSize: 34,
@@ -72,7 +122,19 @@ export const styles = StyleSheet.create({
   description: {
     color: Colors.text, 
     fontSize: 20, 
-  }
+  }, 
+  icon: {
+    color: Colors.primary
+  }, 
+    row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  rowLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
 });
 
 
